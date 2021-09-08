5465ba23e7734cd46c3c5f53cdf45af9270acf9d
Ben Mr. Admin <afisci@gmail.com>
2021-09-08 10:29:51 +0300

    Katman ve Classlar Oluşturuldu

---
+++ EntityLayer/Concrete/Comment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Comment
+    {
+
+        public int CommentID { get; set; }
+
+        public string CommentUserNeme { get; set; }
+
+        public string CommentTitle { get; set; }
+
+        public string CommentContent { get; set; }
+
+        public DateTime CommentDate { get; set; }
+
+        public bool CommentStatus { get; set; }
+
+    }
+}
