de906d185f8cd32456bcea9908a86c2fc4ce1a6f
ahmetyvs <afisci@gmail.com>
2021-10-09 00:48:12 +0300

    46'ya kadar güncel

--- aspnetcorekampi/Controllers/LoginController.cs
+++ aspnetcorekampi/Controllers/LoginController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +10,35 @@ using System.Threading.Tasks;
 
 namespace aspnetcorekampi.Controllers
 {
+    
+    
+    [AllowAnonymous]
     public class LoginController : Controller
     {
+        
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Index(Writer p)
+        {
+            Context c = new Context();
+
+            var datavalue = c.Writers.FirstOrDefault(x => x.WriterMail == p.WriterMail && x.WriterParola == p.WriterParola);
+            if (datavalue != null)
+            {
+                HttpContext.Session.SetString("username", p.WriterMail);
+                return RedirectToAction("Index", "Blog");
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+
     }
 }
--- aspnetcorekampi/Controllers/Register.cs
+++ aspnetcorekampi/Controllers/Register.cs
@@ -3,6 +3,7 @@ using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 using FluentValidation.Results;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,6 +12,8 @@ using System.Threading.Tasks;
 
 namespace aspnetcorekampi.Controllers
 {
+    [AllowAnonymous]
+
     public class Register : Controller
     {
         WriterManager wm = new WriterManager(new EfWriterRepository());
