23126041a391302ffce1f6c9c6c168277d001438
ahmetyvs <afisci@gmail.com>
2021-11-05 17:18:03 +0300

    70'e kadar

--- aspnetcorekampi/Controllers/DashboardController.cs
+++ aspnetcorekampi/Controllers/DashboardController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -10,8 +13,13 @@ namespace aspnetcorekampi.Controllers
     [AllowAnonymous]
     public class DashboardController : Controller
     {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
         public IActionResult Index()
         {
+            Context c = new Context();
+            ViewBag.v1 = c.Blogs.Count().ToString();
+            ViewBag.v2 = c.Blogs.Where(x=>x.WriterID==1).Count().ToString();
+            ViewBag.v3 = c.Categories.Count().ToString();
             return View();
         }
     }
